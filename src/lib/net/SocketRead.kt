1710015e7c4c0513271f9bf131e3b0cc6ec92849
bensoer <ben@soernet.ca>
2016-03-07 17:16:05 -0800

    added SocketRead documentation

--- src/lib/net/SocketRead.kt
+++ src/lib/net/SocketRead.kt
@@ -6,4 +6,9 @@ import java.nio.ByteBuffer
  * Created by bensoer on 04/03/16.
  */
 
+/**
+ * SocketRead is a data object class representing a Read of data from a given socket. It holds the ByteBuffer
+ * containing the data read and an Int representing the number of bytes read from the socket read function. If
+ * the socket was terminated, the bytesRead value should be -1
+ */
 data class SocketRead(val data: ByteBuffer, val bytesRead:Int);
