4fa005eeaffd748738318a9bd4b06c9997d0df42
bensoer <ben@soernet.ca>
2016-03-07 17:12:41 -0800

    added documentation commenting to methods

--- src/lib/net/Select.kt
+++ src/lib/net/Select.kt
@@ -9,6 +9,10 @@ import java.util.concurrent.locks.ReentrantLock
  * Created by bensoer on 02/03/16.
  */
 
+/**
+ * Select is a helper / wrapper class that wraps the Java Selector NIO functionality into a single interactable
+ * class, thus to give the client and outer libraries a more legible and strict API to interact with the selector
+ */
 class Select {
 
     private val selector:Selector = Selector.open();
@@ -16,6 +20,10 @@ class Select {
 
     private var registerServerLock = ReentrantLock();
 
+    /**
+     * waitForEvent is a blocking call that waits for events to occur on the sockets that have been registered
+     * with the Select class
+     */
     fun waitForEvent(): Int{
 
         val readySet:Int = this.selector.select();
@@ -24,10 +32,20 @@ class Select {
 
     }
 
+    /**
+     * getReadyChannels is a helper method used primarily with waitForEvent. When waitForEvent returns, the client
+     * is expected to call this method to get a list of the keys belonging to the channels that have had activity
+     * occur on them and are ready to be interacted with by the client
+     */
     fun getReadyChannels(): Set<SelectionKey> {
         return this.selector.selectedKeys();
     }
 
+    /**
+     * registerChannel registers a passed in channel with the select object so that it is included in the loop
+     * to be checked for events on its socket. The method registers the channel with the ability to listen for
+     * READ events where the client can read from the channel
+     */
     fun registerChannel(channel: AbstractSelectableChannel) : SelectionKey{
         Logger.log("Select - Registering Channel With Select");
 
@@ -42,6 +60,10 @@ class Select {
 
     }
 
+    /**
+     * registerServerChannel registers a ServerSocketChannel with the Select object so that it is included in the
+     * check loop for events on its socket. The channel is registered to listen for ACCEPT events.
+     */
     fun registerServerChannel(channel: ServerSocketChannel) : SelectionKey{
         //ensuring that there is no overlap in this process
         this.registerServerLock.lock();
@@ -60,18 +82,32 @@ class Select {
 
     }
 
+    /**
+     * isNewConnection is a helper method for checking if the passed in key's channel is accepting a connection
+     * OR has the ability to accept connections
+     */
     fun isANewConnection(key: SelectionKey): Boolean{
         return key.isAcceptable();
     }
 
+    /**
+     * hasDataToRead is a helper method for checking if the passed in key's channel has data to be read OR has the
+     * ability to be read from
+     */
     fun hasDataToRead(key: SelectionKey): Boolean{
         return key.isReadable();
     }
 
+    /**
+     * getChannelForKey is a helper method that fetches the associated channel with the given key
+     */
     fun getChannelForKey(key: SelectionKey): SelectableChannel {
         return key.channel();
     }
 
+    /**
+     * getAllKeys is a helper method that returns all keys that have been registered with the select object
+     */
     fun getAllKeys() : MutableSet<SelectionKey>{
         return this.selector.keys();
     }
